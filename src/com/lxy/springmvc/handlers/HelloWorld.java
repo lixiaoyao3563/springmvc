9c8da9edf74b9a95ce70916f15e72cf98dc2d7cf
lixiaoyao3563 <lixiaoyao3563@qq.com>
2018-09-08 09:36:30 +0800

    first commit

---
+++ src/com/lxy/springmvc/handlers/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.lxy.springmvc.handlers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorld {
+	
+	@RequestMapping("/hello")
+	public String hello() {
+		System.out.println("hello");
+		return "success";
+	}
+	
+}
